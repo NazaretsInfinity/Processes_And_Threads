44880dddcd63b50062fb2b024c9542273080ba3b
NazaretsInfinity <157612924+NazaretsInfinity@users.noreply.github.com>
2025-02-01 00:33:55 +0500

    messageBox deleted(wasn't used)

--- HW2_ChildProccess/Program.cs
+++ HW2_ChildProccess/Program.cs
@@ -10,8 +10,7 @@ namespace HW2_ChildProccess
 {
     internal class Program
     {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+       
 
         static void Main(string[] args)
         {
