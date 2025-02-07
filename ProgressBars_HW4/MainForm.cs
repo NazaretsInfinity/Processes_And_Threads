2a7910e00ddc70895c72ca8fa42c37374a947179
NazaretsInfinity <157612924+NazaretsInfinity@users.noreply.github.com>
2025-02-07 17:59:50 +0500

    Horse race

--- ProgressBars_HW4/MainForm.cs
+++ ProgressBars_HW4/MainForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 
 namespace ProgressBars_HW4
 {
@@ -24,38 +25,68 @@ namespace ProgressBars_HW4
 
         private async void ButtonStart_Click(object sender, EventArgs e)
         {
-            activity = !activity;
-            if (!activity)
-            {
-                await task;
-                for (int i = 0; i < bars.Count; ++i)
+#if true1 // dancing bars
+            
+                activity = !activity;
+                if (!activity)
+                {
+                    await task;
+                    for (int i = 0; i < bars.Count; ++i)
                         this.Controls.RemoveByKey("ProgressBar" + i);
                     bars.Clear();
-            }
-            else
-            {
-                for (int i = 0; i < NumericBars.Value; ++i)
-                {
-                    ProgressBar bar = new ProgressBar();
-                    bar.Size = new Size(100, 20); ;
-                    bar.Location = new Point(20, 40 * (i + 1));
-                    bar.Name = $"ProgressBar{i}";
-                    this.Controls.Add(bar);
-                    bars.Add(bar);
                 }
-                //ThreadPool.QueueUserWorkItem(new WaitCallback(FillBars));
-                task = Task.Run(FillBars);
+                else
+                {
+                    for (int i = 0; i < NumericBars.Value; ++i)
+                    {
+                        ProgressBar bar = new ProgressBar();
+                        bar.Size = new Size(100, 20); ;
+                        bar.Location = new Point(20, 40 * (i + 1));
+                        bar.Name = $"ProgressBar{i}";
+                        this.Controls.Add(bar);
+                        bars.Add(bar);
+                    }
+                    //ThreadPool.QueueUserWorkItem(new WaitCallback(FillBars));
+                    task = Task.Run(FillBars); 
+                }
+#endif
+            for (int i = 0; i < NumericBars.Value; ++i)
+            {
+                ProgressBar bar = new ProgressBar();
+                bar.Size = new Size(100, 20); ;
+                bar.Location = new Point(20, 40 * (i + 1));
+                bar.Name = $"ProgressBar{i}";
+                bar.Maximum = 100;
+                this.Controls.Add(bar);
+                bars.Add(bar);
             }
-         
+            foreach(ProgressBar bar in bars)ThreadPool.QueueUserWorkItem(new WaitCallback(HorseRace), bar);
             
+        
         }
 
         //METHODS
         private void FillBars()
+        {  
+            while (activity)
+            
+                foreach (ProgressBar bar in bars)
+                    this.Invoke(new Action(() => bar.Value = new Random().Next(50, 70)));        
+            
+        }
+
+            Random random = new Random();
+        private void HorseRace(object a)
         {
-          while (activity)
-                    foreach (ProgressBar bar in bars)
-                        this.Invoke(new Action(() => bar.Value = new Random().Next(50, 70)));
+           ProgressBar bar = a as ProgressBar;
+           while (bar.Value <100)
+            {
+                int step = random.Next(1, 10);
+                if (bar.Value + step <= 100)
+                this.Invoke(new Action(() => bar.Value += step));
+                Thread.Sleep(100);
+            }
+           
         }
     }
 }
